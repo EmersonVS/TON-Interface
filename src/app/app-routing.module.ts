1ffc4af959b0b5e3d8c0a5c69141c73e4275589e
EmersonVS <eme_valentim@hotmail.com>
2021-02-06 14:32:40 -0300

    feat: control panel component adicionado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ControlPanelComponent } from './core/control-panel/control-panel.component';
 import { LoginComponent } from './core/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'control',
+    component: ControlPanelComponent
   }
 ];
 
---
+++ src/app/core/control-panel/control-panel.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlPanelComponent } from './control-panel.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ControlPanelComponent
+  ],
+  declarations: [
+    ControlPanelComponent
+  ]
+})
+export class ControlPanelModule { }
