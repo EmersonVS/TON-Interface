3bde8688fc7edde6a241751fd9859b670bd0820d
EmersonVS <eme_valentim@hotmail.com>
2021-02-06 14:12:25 -0300

    feat: endpoints para requisições

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  ipPublico: 'http://54.233.207.122:8084',
+  endpoint: {
+    auth: 'user/authenticate',
+    funcionario: 'funcionario/',
+    update: 'funcionario/',
+    delete: 'funcionario/',
+    create: 'createfuncionario/',
+    funcionarios: 'funcionario/all',
+  }
 };
